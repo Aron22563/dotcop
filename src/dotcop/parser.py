dd3a1cac9272c0ef6a8bd2121aa442b3264f3fcb
Aron Tinkl <arontinkl@gmail.com>
2025-10-20 19:40:31 +0200

    feat: separate argument parsing into separate file

--- src/dotcop/parser.py
+++ src/dotcop/parser.py
@@ -1,16 +1,52 @@
 import argparse 
+import pathlib
 from dotcop.utils.logging_setup import Logger
 
 class Parser: 
     def __init__(self): 
         self.logger = Logger.get_logger(__name__)
+        self.init_parser()
+        self.sub_parser()
 
-    def test_arguments(self): 
-        print("test_arguments was called")
-    
     def parse_arguments(self): 
-        try: 
-            test_arguments()
-            call_task()            
+        try:    
+            args = self.parser.parse_args()
+            self.logger.debug(f"Arguments parsed as: {args}")
+            return args
         except Exception as e: 
+            self.logger.critical(f"Argument parsing failed: {e}")
             raise
+
+    def init_parser(self): 
+        self.parser = argparse.ArgumentParser(
+            prog="Dotcop",
+            description="Configuration package manager for versioning and synchronization of Linux configuration files.",
+            epilog="This is currently a work in progress, use with caution..."
+        )
+
+    def sub_parser(self):
+        subparsers = self.parser.add_subparsers(dest="command", required=True)
+
+        subparsers.add_parser("help", help="Show help message")
+        subparsers.add_parser("status", help="Show status")
+        subparsers.add_parser("list", help="List installed configurations")
+        subparsers.add_parser("create", help="Create a new program configuration")
+        subparsers.add_parser("edit", help="Edit an existing program configuration")
+
+        install_parser = subparsers.add_parser(
+                "install",
+                help="Install a configuration",
+                description="Install a configuration package into the system."
+        )
+        install_parser.add_argument("packages", nargs="+", help="Package name to install")
+        install_parser.add_argument("--force", action="store_true", default=False,  help="Force installation")
+        
+        remove_parser = subparsers.add_parser(
+                "remove",
+                help="Remove a configuration",
+                description="Remove a configuration package from the system"
+        )
+        remove_parser.add_argument("packages", nargs="+", help="Package name to remove")
+        remove_parser.add_argument("--force", action="store_true", default=False, help="Force removal")
+
+
--- src/dotcop/setup.py
+++ src/dotcop/setup.py
@@ -4,51 +4,8 @@ from dotcop.config import Config
 from dotcop.parser import Parser
 def main():
     cfg = Config()
+    prs = Parser()
     config_file = cfg.load_configfile()
+    args = prs.parse_arguments()
+    print(args)
 
-    # Initialize argument parsing
-    parser = argparse.ArgumentParser(
-        prog="Dotcop",
-        description="Configuration package manager for versioning and synchronization of Linux configuration files.",
-        epilog="This is currently a work in progress, use with caution..."
-    )
-    subparsers = parser.add_subparsers(dest="command", required=True)
-
-    subparsers.add_parser("help", help="Show help message")
-    subparsers.add_parser("status", help="Show status")
-    subparsers.add_parser("list", help="List installed configurations")
-
-    install_parser = subparsers.add_parser("install", help="Install a configuration")
-    install_parser.add_argument("package", help="Package name to install")
-    install_parser.add_argument("--version", help="Specific version to install")
-    install_parser.add_argument("--force", action="store_true", help="Force reinstall")
-    
-    remove_parser = subparsers.add_parser("remove", help="Remove a configuration")
-    remove_parser.add_argument("package", help="Package name to remove")
-
-    args = parser.parse_args()
-    core=Core()
-    if args.command == "help":
-        parser.print_help()
-    elif args.command == "status":
-        core.status()
-    elif args.command == "list":
-        core.list()
-    elif args.command == "install":
-        core.install(package=args.package, version=args.version, force=args.force)
-    elif args.command == "remove":
-        core.remove(package=args.package)
-    else:
-        parser.print_help()
-
-class Core:
-    # Check update status, vulnerability issues, anything basically
-    def status(self):
-        pass
-    # List installed packages
-    def list(self):
-        pass
-    def install(self, package, version, force):
-        print(package, version, force) 
-    def remove(self, package): 
-        print(package)
