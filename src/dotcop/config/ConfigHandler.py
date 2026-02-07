7e1115790e0f9fce06a7865e5efc833858fc5917
Aron Tinkl <arontinkl@gmail.com>
2026-02-07 11:04:45 +0100

    feat: refactor configuration loading add meta state directory

--- src/dotcop/config/ConfigHandler.py
+++ src/dotcop/config/ConfigHandler.py
@@ -1,6 +1,7 @@
-from dotcop.config.LoadConfigFile import load_config_file
-from dotcop.config.LoadDBFile import load_database_file
-    
+from dotcop.config.load_config_file import load_config_file
+from dotcop.config.load_db_file import load_database_file
+from dotcop.config.load_manifest_dir import load_manifest_dir
+from dotcop.config.load_meta_dir import _load_meta_dir
 def load_dotcop_config():
     try:
         configuration_file = load_config_file()
@@ -15,4 +16,12 @@ def load_dotcop_database():
     except Exception: 
         raise 
     return database_file
+
+def _load_dotcop_meta_directory(): 
+    configuration_file = load_config_file() 
+    try: 
+        meta_directory = _load_meta_dir(configuration_file)
+    except Exception: 
+        raise 
+    return meta_directory
         
