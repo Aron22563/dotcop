4f4235b34b5e2cf405ea616b706c1d9d32298075
Aron Tinkl <arontinkl@gmail.com>
2026-02-18 18:45:06 +0100

    fix: update error raises instead of returning, implement package db updates in file not memory

--- src/dotcop/command/commands/ActivateCommand.py
+++ src/dotcop/command/commands/ActivateCommand.py
@@ -6,6 +6,7 @@ from yaml import YAMLError
 from dotcop.utils.logging_setup import Logger
 from dotcop.config.ConfigHandler import load_dotcop_config
 from dotcop.config.ConfigHandler import load_dotcop_database
+from dotcop.config.ConfigHandler import update_dotcop_database_package
 from dotcop.core.Linker import Linker
 
 logger = Logger.get_logger(__name__)
@@ -14,18 +15,15 @@ class ActivateCommand:
     def run(self, args):
         self.configuration_file = load_dotcop_config()
         self.database_file = load_dotcop_database()
-        for package in args.packages:
-            src_path, dst_path = self._test_package(package)
-            print(f"{src_path} -> {dst_path}")
-
-        for package in args.packages:
-            self._load_package(src_path, dst_path)
+        for package in args.packages: 
+            file_paths = self._test_package(package)
+            self._load_package(file_paths, package)
 
     def _test_package(self, package):
         self._load_db_metadata(package)
         self._test_package_setup(package)
-        src_path, dst_path = self._test_file_paths(package)
-        return src_path, dst_path
+        file_paths = self._test_file_paths(package)
+        return file_paths
 
     def _load_db_metadata(self, package):
         logger.info(f"Activating: {package}")
@@ -33,19 +31,19 @@ class ActivateCommand:
         self.package_metadata = self.database_file['packages'].get(package_name)
         if self.package_metadata['active']:
             logger.error("Package is already active, exiting")
-            return
+            raise
 
     def _test_package_setup(self, package):
         package_folder = Path(self.package_metadata['folder'])
         self.package_path = Path(os.path.expandvars(self.configuration_file['package_path'])) / package_folder
         if not self.package_path.is_dir():
             logger.error(f"Package was not found at expected path: {self.package_path}")
-            return
+            raise FileNotFoundError()
 
         metadata_file_path = self.package_path / "metadata.yaml"
         if not metadata_file_path.is_file():
             logger.error(f"Metadata file not found in folder: {metadata_file_path}")
-            return
+            raise FileNotFoundError()
         try:
             with open(metadata_file_path, "r") as file:
                 self.metadata_file = yaml.safe_load(file)
@@ -56,31 +54,38 @@ class ActivateCommand:
         self.files_folder_path = self.package_path / "files"
         if not self.files_folder_path.is_dir():
             logger.error(f"Files folder not found in: {self.files_folder_path}")
-            return
+            raise FileNotFoundError()
 
     def _test_file_paths(self, package):
+        paths = [] 
         for pair in self.metadata_file["files"]:
             src = pair["from"]
             dst = pair["to"]
             src_path = self.package_path /"files"/ src
             if not src_path.is_file():
                 logger.error(f"File not found: {src_path}")
-                return
+                raise FileNotFoundError()
             dst_path = Path(os.path.expandvars(dst))
-            if dst_path.is_file():
+            if dst_path.exists():
                 logger.error(f"Existing file found: {dst_path}")
-                return
-            return src_path, dst_path
+                raise FileExistsError()
+            paths.append((src_path, dst_path))
+        return paths
 
-    def _load_package(self, src_path, dst_path):
+    def _load_package(self, file_paths, package):
         package_folder = Path(self.package_metadata['folder'])
         linker = Linker(package_folder)
-        self._load_files(linker, src_path, dst_path)
+        for src_path, dst_path in file_paths:
+            logger.info(f"{src_path} -> {dst_path}")
+            self._load_files(linker, src_path, dst_path)
+
         #self._finalize_install(package)
-        self._update_package_db()
+        self._update_package_db(package)
 
     def _load_files(self, linker, src_path, dst_path):
         linker.link(src_path, dst_path)
 
-    def _update_package_db(self):
+    def _update_package_db(self, package):
         self.package_metadata['active'] = True
+        update_dotcop_database_package(package, self.package_metadata)
+        logger.info(f"Package activated: {package}")
