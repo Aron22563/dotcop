c96b14853cd438ba61426111407cca71bc47e682
Aron Tinkl <arontinkl@gmail.com>
2026-03-15 17:25:28 +0100

    fix: use internal logger class instead of python logging in command classes

--- src/dotcop/command/commands/CreateCommand.py
+++ src/dotcop/command/commands/CreateCommand.py
@@ -1,6 +1,15 @@
+<<<<<<< Updated upstream
 import logging 
 logger = logging.getLogger(__name__)
 
 class CreateCommand:
     def __init__(self): 
         logger.debug("CreateCommand Object created")
+=======
+from dotcop.utils.logging_setup import Logger
+
+class CreateCommand:
+    def __init__(self):
+        self.logger = Logger.get_logger(__name__)
+        self.logger.debug("CreateCommand Object created")
+>>>>>>> Stashed changes
--- src/dotcop/command/commands/ListCommand.py
+++ src/dotcop/command/commands/ListCommand.py
@@ -1,6 +1,15 @@
+<<<<<<< Updated upstream
 import logging 
 logger = logging.getLogger(__name__)
 
 class ListCommand:
     def __init__(self): 
         logger.debug("ListCommand Object created")
+=======
+from dotcop.utils.logging_setup import Logger
+
+class ListCommand:
+    def __init__(self):
+        self.logger = Logger.get_logger(__name__)
+        self.logger.debug("ListCommand Object created")
+>>>>>>> Stashed changes
