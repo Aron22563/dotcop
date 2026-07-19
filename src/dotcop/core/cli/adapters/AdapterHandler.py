73101eadc859c7dffb99b30ce3a6549949411634
Aron Tinkl <arontinkl@gmail.com>
2026-07-19 15:10:12 +0200

    refactor: switch validation to dict instead of namespace

--- src/dotcop/core/cli/adapters/AdapterHandler.py
+++ src/dotcop/core/cli/adapters/AdapterHandler.py
@@ -2,24 +2,20 @@ from argparse import Namespace
 
 from dotcop.utils.logging_setup import Logger
 from dotcop.core.cli.adapters.status_adapter import adapt_status_command
-from dotcop.core.cli.adapters.status_adapter import validate_status_args
+from dotcop.core.cli.adapters.status_adapter import validate_maximum_argument_count
 
 logger = Logger.get_logger(__name__)
 
 # This translates raw cli flags into domain objects. It decouples the cli from the execution layer.
 class AdapterHandler:
     def run(self, args):
-        adapted_args = args
         command = args.command
-
-        # Pass only flags to dedicated command adapters.
-        cleaned_args = args
-        del cleaned_args.command
+        adapted_args = args
+        
         match command: 
             case 'status':
-                validate_status_args(cleaned_args)
-                adapted_args = adapt_status_command(cleaned_args)
-        adapted_args = Namespace(command=command, query=adapted_args.query)
+                validate_maximum_argument_count(vars(args))
+                adapted_args = adapt_status_command(vars(args))
         logger.debug("Adapted args: %s", adapted_args)
         return adapted_args
 
--- src/dotcop/core/cli/adapters/status_adapter.py
+++ src/dotcop/core/cli/adapters/status_adapter.py
@@ -7,34 +7,34 @@ logger = Logger.get_logger(__name__)
 
 def adapt_status_command(args):
     """
-    @param args contains a list of all possible flags toggled as true or false.
+    @param args contains a dict of all possible flags toggled true or false.
     """
-    if args.inactive:
+
+    if args['inactive']:
         query = 'inactive'
-    elif args.active:
+    elif args['active']:
         query = 'active'
-    elif args.all:
+    elif args['all']:
         query = 'all'
     else:
         query = 'default_query'
-    adapted_status_args = Namespace(query=query)
+    adapted_status_args = Namespace(command=args['command'], query=query)
     print(adapted_status_args)
     return adapted_status_args
 
-EXPECTED_ARGUMENT_COUNT = 1
-def validate_status_args(args):
+MAX_ARGUMENT_COUNT = 1
+def validate_maximum_argument_count(args):
     """
     Tests whether args contains 0 or the expected amount of true flags.
-    @param args contains a "mutually exclusive" (validated) list of possible flags.
+    @param args contains a "mutually exclusive" (asserted here) dict of possible flags.
     """
-    dict_args = vars(args)
+    
     true_argument_count = 0     
-    for key in dict_args:
-        if dict_args[key]: 
+    for key in args:
+        if key == 'command': 
+            break
+        if args[key]: 
             true_argument_count+=1
-    # Assume default value if no values are found true.
-    if true_argument_count == 0: 
-        true_argument_count +=1
 
-    if true_argument_count != EXPECTED_ARGUMENT_COUNT: 
-        raise UnexpectedArgumentCount(true_argument_count, EXPECTED_ARGUMENT_COUNT)
+    if true_argument_count > MAX_ARGUMENT_COUNT: 
+        raise UnexpectedArgumentCount(true_argument_count, MAX_ARGUMENT_COUNT)
