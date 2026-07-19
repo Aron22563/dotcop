454c44ae3615c5b00ab644aeecc496579810ec61
Aron Tinkl <arontinkl@gmail.com>
2026-07-19 13:57:55 +0200

    refactor: Modularize query handling and OCP

--- src/dotcop/command/commands/StatusCommand.py
+++ src/dotcop/command/commands/StatusCommand.py
@@ -7,17 +7,33 @@ class StatusCommand:
 
     def _group_packages_by_query(self, packages, query):
         selected_packages = set()
-        if query == 'all':
-            selected_packages = set(packages.keys())
-        else:
-            for name, metadata in packages.items():
-                if metadata['status'] == query:
-                    selected_packages.add(name)
+        match query: 
+            case 'all':
+                selected_packages = set(packages.keys())
+            case 'active':
+                selected_packages = self._group_packages_by_status(packages, query)
+            case 'inactive': 
+                selected_packages = self._group_packages_by_status(packages, query)
+            case 'default_query':
+                selected_packages = set(packages.keys())
+        return selected_packages
+
+    def _group_packages_by_status(self, packages, query):
+        """
+        Returns a filtered set of packages by their status
+
+        @param packages contains the dict of packages.
+        @param query contains the status to filter by.
+        """
+        selected_packages = set()
+        for name, metadata in packages.items():
+            if metadata['status'] == query:
+                selected_packages.add(name)
         return selected_packages
 
     def run(self, query):
         database_file = load_dotcop_database()
-        self.logger.debug("StatusCommand executed with: %s", query)
+        self.logger.info("StatusCommand executing with: %s", query)
         selected_packages = self._group_packages_by_query(database_file['packages'], query)
         for package in sorted(selected_packages):
             print(package)
