b1dc7442400899505553c2c8391710bbb5e75e09
Aron Tinkl <arontinkl@gmail.com>
2026-07-22 13:38:48 +0200

    docs: improve string expansion and path handling

--- src/dotcop/config/load_config_file.py
+++ src/dotcop/config/load_config_file.py
@@ -9,43 +9,6 @@ from dotcop.utils.root_finder import ROOT
 
 logger = Logger.get_logger(__name__)
 
-def _test_config_file():
-    # Test if XDG_CONFIG_HOME environment variable exists, if not fail
-    XDG_CONFIG_HOME = os.environ.get("XDG_CONFIG_HOME")
-    if XDG_CONFIG_HOME is None or not Path(XDG_CONFIG_HOME).is_dir():
-        logger.critical(f"XDG_CONFIG_HOME was not found or is incorrect: {XDG_CONFIG_HOME}")
-        raise EnvironmentError()
-
-    # Test if configdir_path exists, if not create it
-    configdir_path = Path(XDG_CONFIG_HOME) / "dotcop"
-    if not configdir_path.is_dir():
-        logger.info(f"No valid configuration directory found at {configdir_path}")
-        try:
-            logger.debug(f"Creating configuration directory at {configdir_path}")
-            os.mkdir(configdir_path)
-        except (FileExistsError, PermissionError):
-            logger.critical(
-                f'Configuration directory "dotcop" could not be created in {XDG_CONFIG_HOME}. This could indicate incorrect access rights.'
-            )
-            raise
-
-    # Test if XDG_CONFIG_HOME/dotcop/dotcop.yaml exists, if not copy default configuration from root/conf/.
-    config_path = configdir_path / "dotcop.yaml"
-    if not config_path.is_file():
-        logger.info(f"No valid configuration file found at {config_path}")
-        src = ROOT / "conf/dotcop.yaml"
-        if not src.is_file():
-            logger.critical(f"Default configuration file was not found in: {src}, please load manually")
-            raise FileNotFoundError()
-        dst = config_path
-        try:
-            logger.debug(f"Copying default configuration from {src} to {dst}")
-            shutil.copyfile(src, dst)
-        except PermissionError:
-            logger.critical(f"Failed to copy default configuration file from {src} to {dst}")
-            raise
-    return config_path
-
 def load_config_file():
     config_path = _test_config_file()
     try:
@@ -55,3 +18,38 @@ def load_config_file():
         logger.critical(f"Failed to parse yaml configuration: {e}")
         raise
     return config_file
+
+def _test_config_file():
+    # Test if XDG_CONFIG_HOME environment variable exists, if not fail
+    XDG_CONFIG_HOME = Path(os.environ.get("XDG_CONFIG_HOME"))
+    if XDG_CONFIG_HOME is None or not XDG_CONFIG_HOME.is_dir():
+        logger.error("XDG_CONFIG_HOME was not found or is incorrect: %s", XDG_CONFIG_HOME)
+        raise EnvironmentError()
+
+    # Test if configdir_path exists, if not create it
+    configdir_path = XDG_CONFIG_HOME / "dotcop"
+    if not configdir_path.is_dir():
+        logger.info("No valid configuration directory found at %s", configdir_path)
+        try:
+            logger.warn("Creating initial configuration directory at %s", configdir_path)
+            os.mkdir(configdir_path)
+        except (FileExistsError, PermissionError):
+            logger.error("Configuration directory could not be created in %s. This could indicate incorrect access rights.", XDG_CONFIG_HOME)
+            raise
+
+    # Test if XDG_CONFIG_HOME/dotcop/dotcop.yaml exists, if not copy default configuration from root/conf/.
+    config_path = configdir_path / "dotcop.yaml"
+    if not config_path.is_file():
+        logger.info("No valid configuration file found at %s", config_path)
+        src = ROOT / "conf/dotcop.yaml"
+        if not src.is_file():
+            logger.error("Default configuration file was not found in: %s, please load manually", src)
+            raise FileNotFoundError()
+        dst = config_path
+        try:
+            logger.debug("Copying default configuration from %s to %s", src, dst)
+            shutil.copyfile(src, dst)
+        except PermissionError:
+            logger.error("Failed to copy default configuration file from %s to %s", src, dst)
+            raise
+    return config_path
