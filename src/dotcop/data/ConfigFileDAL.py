dad81764c9acdc7ca1c1d138a540fc133708c7d8
Aron Tinkl <arontinkl@gmail.com>
2026-07-22 11:35:15 +0200

    feat: implement correct centralized expansion and unified configfile access
    Related: #24
    
    Related: #22

--- src/dotcop/data/ConfigFileDAL.py
+++ src/dotcop/data/ConfigFileDAL.py
@@ -1,11 +1,38 @@
+import os
+from pathlib import Path
+
 from dotcop.config.ConfigHandler import load_config_file
-from dotcop.data.interface.ConfigFileInterface import ConfigFileInterface
 from dotcop.utils.logging_setup import Logger
 
-class ConfigFileDAL(ConfigFileInterface):
+class ConfigFileDAL():
     def __init__(self):
         self.logger = Logger.get_logger(__name__)
 
+    def get_name_field(self) -> str:
+        name_field = self._get_value_by_key('name')
+        return name_field
+
+    def get_log_path(self) -> str:
+        log_path = self._expand_path_from_string(self._get_value_by_key('log_path'))
+        return log_path
+
+    def get_package_folder_path(self) -> Path:
+        package_path = self._expand_path_from_string(self._get_value_by_key('package_path'))
+        return package_path
+
+    def get_package_path(self, package_name) -> Path: 
+        package_path = self.get_package_folder_path()
+        appended_path = package_path / package_name
+        return appended_path
+
+    def get_database_path(self) -> Path:
+        database_path = self._expand_path_from_string(self._get_value_by_key('dotcop_database'))
+        return database_path
+
+    def get_meta_path(self) -> Path:
+        meta_path = self._expand_path_from_string(self._get_value_by_key('dotcop_meta'))
+        return meta_path
+    
     def _load_config_file(self):
         config_file = load_config_file()
         return config_file
@@ -17,18 +44,7 @@ class ConfigFileDAL(ConfigFileInterface):
             return value
         self.logger.error("Value was not found in configuration file: {}", key)
         raise KeyError
-
-    def get_name_field(self) -> str:
-        return self._get_value_by_key('name')
-
-    def get_log_path(self) -> str:
-        return self._get_value_by_key('log_path')
-
-    def get_package_path(self) -> str:
-        return self._get_value_by_key('package_path')
-
-    def get_database_path(self) -> str:
-        return self._get_value_by_key('dotcop_database')
-
-    def get_meta_path(self) -> str:
-        return self._get_value_by_key('dotcop_meta')
+        
+    def _expand_path_from_string(self, string_path) -> Path: 
+        path = Path(os.path.expandvars(string_path))
+        return path
