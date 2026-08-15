d076005dd7239bfc2bf3f4c3445bb8f77fcee882
Aron Tinkl <arontinkl@gmail.com>
2026-08-15 19:00:10 +0200

    feat: implement centralized envvar expansion

--- src/dotcop/config/load_config_file.py
+++ src/dotcop/config/load_config_file.py
@@ -6,7 +6,7 @@ from yaml import YAMLError
 
 from dotcop.utils.logging_setup import Logger
 from dotcop.utils.root_finder import ROOT
-from dotcop.config.exceptions.InvalidEnvironmentVariable import InvalidEnvironmentVariable
+from dotcop.utils.expand_path import expand_path_from_string
 
 logger = Logger.get_logger(__name__)
 
@@ -21,29 +21,27 @@ def load_config_file():
     return config_file
 
 def _test_config_file():
-    # Test if XDG_CONFIG_HOME environment variable exists, if not fail
-    expanded_variable = os.environ.get("XDG_CONFIG_HOME")
-    if expanded_variable is None:
-        raise InvalidEnvironmentVariable("XDG_CONFIG_HOME")
-
-    XDG_CONFIG_HOME = Path(os.environ.get("XDG_CONFIG_HOME"))
-    if XDG_CONFIG_HOME is None or not XDG_CONFIG_HOME.is_dir():
-        logger.error("XDG_CONFIG_HOME was not found or is incorrect: %s", XDG_CONFIG_HOME)
+    try:
+        config_home = expand_path_from_string("$XDG_CONFIG_HOME")
+    except PathExpansionFailure:
+        logger.error("XDG_CONFIG_HOME environment variable is not set")
+        raise
+    if not config_home.is_dir():
+        logger.error("XDG_CONFIG_HOME environment variable does not point to a directory")
         raise EnvironmentError()
 
-    # Test if configdir_path exists, if not create it
-    configdir_path = XDG_CONFIG_HOME / "dotcop"
-    if not configdir_path.is_dir():
-        logger.info("No valid configuration directory found at %s", configdir_path)
+    dotcop_config_path = config_home / "dotcop"
+    if not dotcop_config_path.is_dir():
+        logger.info("No valid configuration directory found at %s", dotcop_config_path)
         try:
-            logger.warn("Creating initial configuration directory at %s", configdir_path)
-            os.mkdir(configdir_path)
-        except (FileExistsError, PermissionError):
-            logger.error("Configuration directory could not be created in %s. This could indicate incorrect access rights.", XDG_CONFIG_HOME)
+            logger.warn("Creating initial configuration directory at %s", dotcop_config_path)
+            os.mkdir(dotcop_config_path)
+        except PermissionError:
+            logger.error("Dotcop configuration directory could not be created in %s. This could indicate incorrect access rights.", dotcop_config_path)
             raise
 
     # Test if XDG_CONFIG_HOME/dotcop/dotcop.yaml exists, if not copy default configuration from root/conf/.
-    config_path = configdir_path / "dotcop.yaml"
+    config_path = dotcop_config_path / "dotcop.yaml"
     if not config_path.is_file():
         logger.info("No valid configuration file found at %s", config_path)
         src = ROOT / "conf/dotcop.yaml"
@@ -52,7 +50,7 @@ def _test_config_file():
             raise FileNotFoundError()
         dst = config_path
         try:
-            logger.debug("Copying default configuration from %s to %s", src, dst)
+            logger.warn("Copying default configuration from %s to %s", src, dst)
             shutil.copyfile(src, dst)
         except PermissionError:
             logger.error("Failed to copy default configuration file from %s to %s", src, dst)
